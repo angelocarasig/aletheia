3ae92ce3fdff4a65efcbb90bd0ac77fc1661736c
Angelo Carasig <angelocarasig15@gmail.com>
2026-07-18 16:38:49 +1000

    Initial Commit

---
+++ aletheia/aletheiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  aletheiaApp.swift
+//  aletheia
+//
+//  Created by Angelo Carasig on 18/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct aletheiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
